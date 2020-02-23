4dcdc5aec5ebc23e2a2c48ff949ab2b76f16ca27
Artem <temo4kinn@gmail.com>
2020-02-23 20:43:12 +0300

    Подготовка макета

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, abort
+import json
 
 app = Flask(__name__)
 
@@ -7,17 +8,39 @@ def index():
     return render_template("index.html")
 
 @app.route('/goals/<goal>')
-def departure(goal):
+def goals(goal):
     return render_template("goal.html")
 
 
 @app.route('/profiles/<uin>')
-def tour(uin):
+def profiles(uin):
     return render_template("profile.html")
+
+
+@app.route('/request')
+def request():
+    return render_template("request.html")
+
+
+@app.route('/request_done')
+def request_done():
+    return render_template("request_done.html")
+
+
+@app.route('/booking')
+def booking():
+    return render_template("booking.html")
+
+
+@app.route('/booking_done')
+def booking_done():
+    return render_template("booking_done.html")
+
 
 @app.errorhandler(404)
 def render_not_found(error):
     return "Ничего не нашлось! Вот неудача, отправляйтесь на главную!", 404
+
 
 @app.errorhandler(500)
 def render_server_error(error):
@@ -29,5 +52,11 @@ def not_found(e):
     output = render_template("404.html", title=title, subtitle=subtitle, departures=departures, description=)
     return output
 '''
+
+with open('data.json', 'r') as f:
+    team = json.loads(f.read())
+
+print(team)
+
 if __name__ == '__main__':
     app.run()
